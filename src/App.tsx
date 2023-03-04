71144006cd5c704764853a4e0a8d7cfd9d18e92b
bvenceslas <bvenceslas@gmail.com>
2023-03-04 08:32:33 +0200

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-2xl text-left text-red-500">App</h1>;
+  return <h1 className="text-3xl text-left text-blue-500">React App</h1>;
 }
 
 export default App;
