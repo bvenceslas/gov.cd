5875030d7d07936973d31e9c0822442a328d1d9d
bvenceslas <bvenceslas@gmail.com>
2023-05-15 10:20:30 +0200

    mobile version hero

--- src/components/common/Hero/index.tsx
+++ src/components/common/Hero/index.tsx
@@ -2,24 +2,28 @@ import searchIcon from "src/assets/icons/search.svg";
 
 const Hero: React.FunctionComponent = () => {
   return (
-    <div className="w-full bg-hero-pattern h-[400px] bg-cover px-5 border border-green-700">
-      <p className="text-2xl text-white shadow-sm ">
-        Welcome to <br />{" "}
-        <span className="text-4xl font-bold font-poppins">DR Congo</span>
-      </p>
+    <div className="w-full bg-hero-pattern h-[450px] bg-cover px-5 border border-green-700">
+      <div className="welcome-wrapper mx-5 mt-32">
+        <p className="welcome-text text-2xl text-white font-bold font-poppins">
+          Welcome to
+        </p>
+        <p className="welcome-text text-5xl mt-8 text-white font-poppins font-extrabold">
+          DRCongo.
+        </p>
 
-      <div className="absolute w-full  mt-[150px] flex justify-center ">
-        <input
-          type="text"
-          placeholder="What are you looking for?"
-          className=" w-[70%] h-[40px] rounded-sm text-sm px-2"
-        />
-        <img
-          src={searchIcon}
-          alt="search icon"
-          width={40}
-          className="bg-[#098DCD]"
-        />
+        <div className="search-wrapper mt-[50px] flex justify-center">
+          <input
+            type="text"
+            placeholder="what are you looking for?"
+            className="w-[90%] h-[40px] rounded-sm text-sm px-2"
+          />
+          <img
+            src={searchIcon}
+            alt="search icon"
+            width={40}
+            className="bg-[#098DCD] cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
