93829300aeab98ef94ffae4e9e7d49d011d517f6
bvenceslas <bvenceslas@gmail.com>
2023-06-02 10:15:02 +0200

    description component with mobile and web responsiveness

--- src/components/Description/cards.tsx
+++ src/components/Description/cards.tsx
@@ -5,11 +5,11 @@ type Props = {
 
 const Cards: React.FunctionComponent<Props> = ({ title, description }) => {
   return (
-    <div className="card-wrapper bg-white w-full min-h-[84px] p-8 border-b-2">
-      <div className="card-title text-xl text-[#098DCD] font-poppins font-bold mb-4">
+    <div className="card-wrapper bg-white w-full min-h-[84px] p-8 border-b-2 md:min-w-[100px] md:border-r-2">
+      <div className="card-title text-xl text-[#098DCD] font-poppins font-bold mb-4 md:text-2xl">
         {title}
       </div>
-      <p className="card-desc text-sm">{description}</p>
+      <p className="card-desc text-sm md:text-base">{description}</p>
     </div>
   );
 };
